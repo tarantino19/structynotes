1577622c4e0a1c157219922e915aa03c5cfd7136
Red Tolentino <tolentinored19@gmail.com>
2026-05-03 10:58:54 +0800

    rec

---
+++ recursion/basicrecursion.js
@@ -0,0 +1,13 @@
+//common pattern - get first element, do something with it, call function again with rest of elements
+//get first element - numbers[0]
+//slice array
+//return. base case as the number ti return
+
+
+//if array - find base case - if (array.length === 0) return 0
+//use .length if params/args i array
+//else return first element + function again with rest of elements
+//rest of elements - slice(1) - creates new array without first element
+//each slice gets a new array - and then numbers[0] gets the first property of that new array
+//the slice is the key here
+//
--- recursion/sumnumbersrecursive.js
+++ recursion/sumnumbersrecursive.js
@@ -12,6 +12,11 @@ const sumNumbersRecursive = (numbers) => {
   return numbers[0] + sumNumbersRecursive(numbers.slice(1))
 };
 
+//common pattern - get first element, do something with it, call function again with rest of elements
+//get first element - numbers[0]
+//slice array
+//return. base case as the number ti return
+
 //each slice gets a new array - and then numbers[0] gets the first property of that new array
 
 
